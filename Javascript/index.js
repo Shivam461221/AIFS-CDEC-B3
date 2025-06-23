d098255bb6ed6ada9964d4df5b917fc04520cfaa
Shivam Lowanshi <shivamlowanshi@gmail.com>
2025-06-23 21:02:28 +0530

    JS intro

---
+++ Javascript/index.js
@@ -0,0 +1,29 @@
+// console.log('Hello from JS');
+
+
+// function showAlert(){
+//    document.body.style.backgroundColor = 'red';
+// }
+
+//Variables, Datatypes
+
+//Variables - Containers for holding data during runtime 
+// calculator = sum two numbers , x=10, y=20
+// User login form - name, 
+
+// x = 10;  - number
+// Primitive, Non- Primitive
+// Primitive - number, string, boolean,null, undefined
+// name = 'Shivam', password = "shivam@123"  - string
+// isMarried = true;  boolean 
+
+// isPassed = null ;  
+// x;   undefined 
+
+
+
+// dynamically types language
+
+
+
+
