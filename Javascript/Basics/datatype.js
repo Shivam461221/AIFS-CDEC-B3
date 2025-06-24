6564ab9c3f3a0606c405dd111308584d3a46a9c4
Shivam Lowanshi <shivamlowanshi@gmail.com>
2025-06-24 20:57:23 +0530

    Variables, datatypes, operators

---
+++ Javascript/Basics/datatype.js
@@ -0,0 +1,52 @@
+
+console.log('Hello from JS');
+
+//Varibles
+
+// keyword - predefined word which have some meaning
+
+//let, const, var
+
+// var  - traditional
+
+// var name = "Shivam";  
+// console.log(name);
+// name = 'Sagar';
+// console.log(name);
+// var x;  //declaratrion
+// x=10; //initialization
+// var name = 'Megha';
+// console.log(name);
+
+//let -  ES6 version
+//let, const - block scoped
+// let , const - not reclared
+
+let x = 10;
+console.log(x);
+
+{
+   let y = 20;
+   console.log(x, 'Inside block',y); 
+}
+
+//console.log(y);
+
+x = 25;
+console.log(x);
+
+//const - contant declaration
+// const can not be redeclare and reassign
+const pi = 3.14;
+
+console.log(pi);
+
+
+//pi = 3.1432;
+
+
+
+
+
+
+
